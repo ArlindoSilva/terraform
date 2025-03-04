785837f5dd233a0203535a262e1edf4dbc74905e
ArlindoSilva <arlindoas2@gmail.com>
2025-03-04 13:43:44 -0300

    Adicionando chave ssh e firewall ao droplet

--- main.tf
+++ main.tf
@@ -17,8 +17,32 @@ provider "digitalocean" {
 }
 
 resource "digitalocean_droplet" "vm_aula_terraform" {
-  image  = "ubuntu-22-04-x64"
-  name   = "vm-aula-terraform"
-  region = "nyc1"
-  size   = "s-1vcpu-1gb"
+  image    = "ubuntu-22-04-x64"
+  name     = "vm-aula-terraform"
+  region   = "nyc1"
+  size     = "s-1vcpu-1gb"
+  ssh_keys = [data.digitalocean_ssh_key.ssh_key.id]
+}
+
+data "digitalocean_ssh_key" "ssh_key" {
+  name = "aula-terraform"
+}
+
+resource "digitalocean_firewall" "firewall_aula" {
+  name = "firewall-aula"
+
+  droplet_ids = [digitalocean_droplet.vm_aula_terraform.id]
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "22"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "tcp"
+    port_range            = "22"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
 }
