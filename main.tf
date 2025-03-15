c39bd9e7d8053b4ebadd77d2e313a44d9ff96272
ArlindoSilva <arlindoas2@gmail.com>
2025-03-15 19:23:40 -0300

    Variables: 'map'

--- main.tf
+++ main.tf
@@ -1,10 +1,15 @@
 resource "local_file" "foo" {
-  content  = "O valor que vou passar de máquina é ${var.conteudo[0]}"
+  content  = "O valor que vou passar de máquina é ${var.conteudo["pequena"]}"
   filename = "./arquivo.txt"
 }
 
 variable "conteudo" {
-  default = ["Maquina01", "maquina02", "Máquina03"]
-  type = list(string)
+  default = {
+    "pequena" = "s-1vcpu-2gb"
+    "media" = "s-1vcpu-4gb"
+    "grande" = "s-1vcpu-8gb"
+  }
+  #Posso colocar o tipo de dado do map, se quiser. Ex: type = map(string)
+  type = map
   description = "Conteúdo que vai para o arquivo."
 }
---
+++ terraform.tfvars
@@ -0,0 +1,5 @@
+conteudo = {
+    "pequena" = "s-1vcpu-2gb"
+    "media" = "s-1vcpu-4gb"
+    "grande" = "s-1vcpu-8gb"
+  }
