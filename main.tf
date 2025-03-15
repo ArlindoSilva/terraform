f89cd8349f729684f7c8f62165da3f68aaf5c5de
ArlindoSilva <arlindoas2@gmail.com>
2025-03-15 19:11:27 -0300

    Variables 'List'

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 resource "local_file" "foo" {
-  content  = var.conteudo
+  content  = "O valor que vou passar de máquina é ${var.conteudo[0]}"
   filename = "./arquivo.txt"
 }
 
 variable "conteudo" {
-  default = false
-  type = bool
+  default = ["Maquina01", "maquina02", "Máquina03"]
+  type = list(string)
   description = "Conteúdo que vai para o arquivo."
 }
