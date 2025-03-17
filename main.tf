0505e3375ecfec0f52a4e64e05a728f84295b181
ArlindoSilva <arlindoas2@gmail.com>
2025-03-17 17:56:47 -0300

    Meta arguments: 'for each'

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
 
 resource "local_file" "arquivo" {
-  content  = "Esse é o conteúdo do arquivo - ${count.index + 1 } "
-  filename = "./arquivo - ${count.index + 1 }.txt"
-  count = var.contador
+  content  = "Esse é o conteúdo do arquivo - ${each.key} "
+  filename = "./arquivo - ${each.key}.txt"
+  for_each = var.contador
 }
 
 variable "contador" {
-  default = 5
+  default = ["Maquina 01", "Maquina 02", "Maquina 03"]
+  type = set(string)
 } 
 
-# Com o count é possível criar uma estrututa de repetição. 
-
-# Pode ser passado uma variável como no exemplo, ou passar um valor fixo. O " + 1 " é para ele não começar do Zero.  
+# O for each faz uma varredura em um set ou map para criar os resources. 
+  
