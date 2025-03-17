f4ac5f8f8557987517ec7cdaa8bafd28fbd85810
ArlindoSilva <arlindoas2@gmail.com>
2025-03-17 16:09:23 -0300

    Meta arguments: 'depends_on'

--- main.tf
+++ main.tf
@@ -1,28 +1,11 @@
-variable "conteudo_string" {
-  type        = string
-  description = "Conteúdo que vai para o arquivo."
+
+resource "local_file" "arquivo" {
+  content  = "Esse é o conteúdo do arquivo 1"
+  filename = "./arquivo1.txt"
+  depends_on = [ local_file.outro_arquivo ]
 }
 
-variable "lista_nomes" {
-  type        = list(string)
-  description = "Lista de nomes."
+resource "local_file" "outro_arquivo" {
+  content  = "Esse é o conteúdo do arquivo 2"
+  filename = "./arquivo2.txt"
 }
-
-resource "local_file" "arquivo_list" {
-  content  = "Os nomes definidos foram ${join(", ", var.lista_nomes)} "
-  filename = "arquivo-list.txt"
-}
-
-resource "local_file" "arquivo_string" {
-  content  = var.conteudo_string
-  filename = "arquivo-string.txt"
-}
-
-#Também é possível criar um 'arquivo.tfvars' e setar esse aquivo quando for passar o comando apply, ex:
-# $ terraform apply -var-file=arquivo.tfvars
-
-# A terceira forma é passar a variável com o valor quando for usar o apply, ex:
-# $ terraform apply -var="conteudo_string=Teste de variável passada pelo terminal" -var='lista_nomes=["Maquina05", "Maquina06"]'
-
-#Quando a variável passada direto na linha de comando ou com arquivo var-file, sobrescreve as demais formas. 
-#A segundo forma na prioridade de execução é com arquivo .tfvars e por último utilizando TF_VAR_nome_variavel
